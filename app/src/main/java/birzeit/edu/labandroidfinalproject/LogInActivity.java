f2e0ad225d428ca2592c1cb1be90cc3db1e65975
SalsabeelH <1180771@student.birzeit.edu>
2023-01-19 20:42:05 +0200

    project structure init

---
+++ app/src/main/java/birzeit/edu/labandroidfinalproject/LogInActivity.java
@@ -0,0 +1,46 @@
+package birzeit.edu.labandroidfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private Button logInBtn;
+    private Button goToSignUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        extracted();
+
+    }
+
+    private void extracted() {
+        logInBtn = findViewById(R.id.log_in_btn);
+        goToSignUpBtn = findViewById(R.id.go_to_sign_up_btn);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LogInActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        goToSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/birzeit/edu/labandroidfinalproject/SignUpActivity.java
@@ -0,0 +1,32 @@
+package birzeit.edu.labandroidfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+    private Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        extracted();
+    }
+
+    private void extracted() {
+        signUpBtn = findViewById(R.id.sign_up_btn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignUpActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
